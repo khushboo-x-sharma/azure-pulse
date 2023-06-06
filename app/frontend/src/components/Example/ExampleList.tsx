39688a1b8df273b634dc35c49c54bcb51b436583
khushboo <112087816+khushboo-x-sharma@users.noreply.github.com>
2023-06-06 23:31:33 +0200

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -8,12 +8,9 @@ export type ExampleModel = {
 };
 
 const EXAMPLES: ExampleModel[] = [
-    {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
-    },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What is openAI?", value: "What is openAI?" },
+    { text: "How can I migrate my data from On-Prem Storage?", value: "How can I migrate my data from On-Prem Storage?" },
+    { text: "Is Microsoft Fabric GA?", value: "Is Microsoft Fabric GA?" }
 ];
 
 interface Props {
